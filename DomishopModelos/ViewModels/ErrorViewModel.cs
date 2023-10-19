b889189aa8bc3b12809642fa74665cf0a1ee2049
Will360gp <will@gmail.com>
2023-10-18 20:13:26 -0400

    Paquetes actualizados 6.0.20

---
+++ DomishopModelos/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DomishopModelos.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
